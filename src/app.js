5cfe9e1441bcfad58911abce831e89241ac0d799
afantiniv <fantiniv95@gmail.com>
2022-11-11 02:31:23 +0000

    resuelto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,73 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generarCarta(valores, naipes);
 };
+
+let valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+let naipes = ["corazon", "diamantes", "trebol", "espadas"];
+
+function aleatorioArreglo(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function crearDivClass(clase) {
+  let nuevoDiv = document.createElement("div");
+  nuevoDiv.setAttribute("class", clase);
+  console.log(nuevoDiv);
+  return nuevoDiv;
+}
+
+function agregarDivs(divPadre, divHijos) {
+  for (let i = 0; i < divHijos.length; i++) {
+    divPadre.appendChild(divHijos[i]);
+  }
+}
+
+function generarCarta(x, y) {
+  let valor = aleatorioArreglo(x);
+  let palo = aleatorioArreglo(y);
+
+  let tipoCarta = "carta" + " " + palo;
+  let divCarta = crearDivClass(tipoCarta);
+  let divHeader = crearDivClass("carta-icon-header");
+  let divFooter = crearDivClass("carta-icon-footer");
+
+  let divValor = crearDivClass("carta-val");
+
+  if (valor === "A") {
+    divValor = crearDivClass("a-carta-val");
+    divHeader.classList.remove("carta-icon-header");
+    divHeader = crearDivClass("a-carta-header");
+    divHeader.innerHTML = valor;
+
+    divFooter.classList.remove("carta-icon-footer");
+    divFooter = crearDivClass("a-carta-footer");
+    divFooter.innerHTML = valor;
+  } else {
+    divValor.innerHTML = valor;
+  }
+
+  let divsHijos = [divHeader, divValor, divFooter];
+  agregarDivs(divCarta, divsHijos);
+
+  let divWrapper = crearDivClass("wrapper");
+  let divBody = document.getElementsByTagName("body")[0];
+
+  agregarDivs(divWrapper, [divCarta]);
+  agregarDivs(divBody, [divWrapper]);
+}
